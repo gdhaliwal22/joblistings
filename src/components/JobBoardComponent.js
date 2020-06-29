43644c162f2bd2424664fdb2ce601b8916ce2d0f
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-06-29 14:42:45 -0700

    mobile responsive breakpoint 600

--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -45,35 +45,42 @@ const JobBoardComponent = ({
   return (
     <div
       className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${
-        featured && "border-l-4 border-black border-solid border-teal-500"
-      }`}
+        featured && "border-l-4 border-solid border-teal-500"
+      } sm:flex-row`}
     >
       <div>
-        <img className="-mt-16 mb-4 w-20 h-20" src={logo} alt={company} />
+        <img
+          className="-mt-16 mb-4 w-20 h-20 sm:h-24 sm:w-24 sm:my-0"
+          src={logo}
+          alt={company}
+        />
       </div>
       <div className="flex flex-col justify-between ml-2">
         <h3 className="font-bold text-teal-500">
           {company}
           {isNew && (
-            <span className="text-teal-100 bg-teal-500 font-bold m-2 py-1 px-2 rounded-full">
+            <span className="text-teal-100 bg-teal-500 font-bold m-2 py-1 px-2 rounded-full uppercase text-sm">
               New
             </span>
           )}
           {featured && (
-            <span className="bg-gray-800 text-white font-bold m-2 py-1 px-2 rounded-full">
+            <span className="bg-gray-800 text-white font-bold m-2 py-1 px-2 rounded-full uppercase text-sm">
               Featured
             </span>
           )}
         </h3>
-        <h2 className="font-bold text-xl">{position}</h2>
+        <h2 className="font-bold text-xl my-2">{position}</h2>
         <p className="text-gray-700">
           {postedAt} · {contract} · {location}
         </p>
       </div>
-      <div className="flex items-center mt-4 pt-4 border-t-2 border-gray-200 border-solid">
+      <div
+        className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 
+      border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0"
+      >
         {tags
           ? tags.map((tag) => (
-              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
+              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">
                 {tag}
               </span>
             ))
