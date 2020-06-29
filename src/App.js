5003637e5459cfdf4007a101da826d5d47218b6b
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-06-29 11:43:22 -0700

    job board component rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import data from "./assets/data.json";
+import JobBoardComponent from "./components/JobBoardComponent";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      {jobs.length === 0 ? (
+        <p>Jobs are fetching...</p>
+      ) : (
+        jobs.map((job) => <JobBoardComponent job={job} key={job.id} />)
+      )}
     </div>
   );
 }
 
 export default App;
+
+//TODOs
+// 1. Study the design & json
+// 2. Create Job Board Component
+// 3. Get the data from JSON
+// 4. Pass down the date to the JBC
+// 5. Stlye it
+// 6. Filter component
+// 7. Filter the data
---
+++ src/components/JobBoardComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const JobBoardComponent = () => <div>Hi I am Job board</div>;
+
+export default JobBoardComponent;
